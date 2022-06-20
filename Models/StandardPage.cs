402fb04f5ed477390648b708036f2b3e9e8eb130
a.chesnokov <chesnokov_200@mail.ru>
2022-06-20 09:56:21 +0300

    first commit

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace NorthWindCMS.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
